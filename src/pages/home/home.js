8a9b7f9ab9241ea7f8e23b397e287eea6105f5d5
Hariom Tomar <68967590+harrybhai123@users.noreply.github.com>
2023-06-11 02:58:57 +0530

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -10,7 +10,7 @@ const Home = () => {
     const [ popularMovies, setPopularMovies ] = useState([])
 
     useEffect(() => {
-        fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=Your_API_Key&language=en-US")
         .then(res => res.json())
         .then(data => setPopularMovies(data.results))
     }, [])
